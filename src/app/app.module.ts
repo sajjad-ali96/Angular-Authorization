8c6557dcb404a79ed28430d2b01c97812c578023
sajjad-ali96 <sajjad.casanova@gmail.com>
2020-09-05 19:07:02 +0500

    project initiated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,64 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { EventsComponent } from './events/events.component';
+import { SpecialEventsComponent } from './special-events/special-events.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { AuthService } from './auth.service';
+import { EventService } from './event.service';
+import { AuthGuard } from './auth.guard';
+import { TokenInterceptorService } from './token-interceptor.service';
+
+const appRoutes: Routes = [
+  {
+  path: '',
+  redirectTo: '/events',
+  pathMatch: 'full'
+},
+{
+  path: 'events',
+  component: EventsComponent
+},
+{
+  path: 'special',
+  component: SpecialEventsComponent
+},
+{
+  path: 'login',
+  component: LoginComponent
+},
+{
+  path: 'register',
+  component: RegisterComponent
+}];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    EventsComponent,
+    SpecialEventsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [AuthService, AuthGuard, EventService,
+  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: TokenInterceptorService,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
